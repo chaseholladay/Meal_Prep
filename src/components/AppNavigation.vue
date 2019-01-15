ecb819c8b690cfedf37739ee7678d091a43bb4fe
Chase Holladay <chaseholladay@gmail.com>
2019-01-15 14:55:57 -0600

    responsive nav bar for small screens

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,12 +1,13 @@
 <template>
-        <v-toolbar app color="brown darken-4" dark>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-            <v-btn flat>Menu</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn flat>SIGN IN</v-btn>
-            <v-btn color="brown lighten-3">JOIN</v-btn>
-        </v-toolbar>
+       <v-toolbar app color="brown darken-4" dark>
+    <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+    <v-spacer class="hidden-md-and-up"></v-spacer>
+    <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+    <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
+    <v-spacer class="hidden-sm-and-down"></v-spacer>
+    <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
+    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+</v-toolbar>
 </template>
 
 <script>
