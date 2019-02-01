4de31a1f323b5a13ba28e3f7a467e1280ebb1852
Chase Holladay <chaseholladay@gmail.com>
2019-01-31 22:01:14 -0600

    working on app nav

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -36,10 +36,10 @@ export default {
             <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-            <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
+            <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
-            <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+            <v-btn flat class="hidden-sm-and-down" to="/signin">SIGN IN</v-btn>
+            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join">JOIN</v-btn>
         </v-toolbar>
     </span>
 </template>
